c27bfee46c3543c08e0ae5bbf5c799ed4bc3ad70
HamiltonFRJ <hamilton.franc50@gmail.com>
2022-12-02 09:39:28 -0300

    js5-expressoes

---
+++ Unid1/JavaScript/script5.js
@@ -0,0 +1,16 @@
+// Expressões
+const grupoA = 100;
+const grupoB = 200;
+
+// if (grupoA > grupoB) {
+//   console.log('Grupo A ganhou');
+// } else {
+//   console.log('Grupo B ganhou');
+// }
+
+const vencedor = grupoA > grupoB ? 'Grupo A venceu' : 'Grupo B venceu';
+
+const active = true;
+const button = active && 'Botão está ativo';
+
+console.log(button);
