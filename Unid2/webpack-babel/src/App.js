a2901fe3dc7a7bb7c83a3be03aa11cc81df0010c
HamiltonFRJ <hamilton.franc50@gmail.com>
2023-01-08 22:13:16 -0300

    webpack-babel 2

--- Unid2/webpack-babel/src/App.js
+++ Unid2/webpack-babel/src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import './App.css';
+
+// const App = () => {
+//   return React.createElement(
+//     'a',
+//     { href: 'https://www.origamid.com' },
+//     'Origamid',
+//   );
+// };
 
 const App = () => {
-  return React.createElement(
-    'a',
-    { href: 'https://www.origamid.com' },
-    'Origamid',
-  );
+  return <a href="https://www.origamid.com">Hamilton</a>;
 };
 
 export default App;
