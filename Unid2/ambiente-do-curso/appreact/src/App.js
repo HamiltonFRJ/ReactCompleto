7e6f4d705f66698de090b6dd88f222e9a10f3f5e
HamiltonFRJ <hamilton.franc50@gmail.com>
2023-01-11 21:48:53 -0300

    JSX 2

--- Unid2/ambiente-do-curso/appreact/src/App.js
+++ Unid2/ambiente-do-curso/appreact/src/App.js
@@ -1,13 +1,48 @@
 import React from 'react';
 
+const luana = {
+  cliente: 'Luana',
+  idade: 27,
+  compras: [
+    { nome: 'Notebook', preco: 'R$ 2500' },
+    { nome: 'Geladeira', preco: 'R$ 3000' },
+    { nome: 'Smartphone', preco: 'R$ 1500' },
+  ],
+  ativa: true,
+};
+
+const mario = {
+  cliente: 'Mario',
+  idade: 31,
+  compras: [
+    { nome: 'Notebook', preco: 'R$ 2500' },
+    { nome: 'Geladeira', preco: 'R$ 3000' },
+    { nome: 'Smartphone', preco: 'R$ 1500' },
+    { nome: 'Guitarra', preco: 'R$ 3500' },
+  ],
+  ativa: false,
+};
+
 const App = () => {
-  const nome = 'Hamilton';
+  const dados = luana;
+
+  const total = dados.compras
+    .map((item) => Number(item.preco.replace('R$ ', '')))
+    .reduce((a, b) => a + b);
+
   return (
-    <>
-      <p>{nome}</p>
-      <label htmlFor="nome">Nome</label>
-      <input type="text" id="nome" />
-    </> // React.Fragment
+    <div>
+      <p>Nome: {dados.cliente}</p>
+      <p>Idade: {dados.idade}</p>
+      <p>
+        Situação:{' '}
+        <span style={{ color: dados.ativa ? 'green' : 'red' }}>
+          {dados.ativa ? 'Ativa' : 'Inativa'}
+        </span>
+      </p>
+      <p>Total Gasto: R$ {total}</p>
+      {total > 10000 && <p>Gasto elevado!</p>}
+    </div>
   );
 };
 
