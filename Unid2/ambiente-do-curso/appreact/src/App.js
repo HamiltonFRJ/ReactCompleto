b9fdc255509111ab8cb67bf2169c459c596d9a6c
HamiltonFRJ <hamilton.franc50@gmail.com>
2023-01-18 20:48:09 -0300

    useState1.2

--- Unid2/ambiente-do-curso/appreact/src/App.js
+++ Unid2/ambiente-do-curso/appreact/src/App.js
@@ -1,15 +1,22 @@
 import React from 'react';
 
 const App = () => {
-  const ativoHook = React.useState(false);
+  const [ativo, setAtivo] = React.useState(false);
+  const [dados, setDados] = React.useState({ nome: 'Hamilton', idade: '21' });
 
-  console.log(ativoHook);
-  // function handleClick() {
-  //   ativo = !ativo;
-  //   console.log(ativo);
-  // }
+  function handleClick() {
+    setAtivo(!ativo);
+    setDados({ ...dados, faculdade: 'Possui Faculdade' });
+  }
 
-  return <div></div>;
+  return (
+    <div>
+      <p>{dados.nome}</p>
+      <p>{dados.idade}</p>
+      <p>{dados.faculdade}</p>
+      <button onClick={handleClick}>{ativo ? 'Ativo' : 'Inativo'}</button>
+    </div>
+  );
 };
 
 export default App;
