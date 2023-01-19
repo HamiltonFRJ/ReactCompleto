1105fcc9da93c58402b331cec0990bfba6699ec6
HamiltonFRJ <hamilton.franc50@gmail.com>
2023-01-19 13:03:35 -0300

    useState1.3

--- Unid2/ambiente-do-curso/appreact/src/App.js
+++ Unid2/ambiente-do-curso/appreact/src/App.js
@@ -1,20 +1,13 @@
 import React from 'react';
+import ButtonModal from './ButtonModal';
+import Modal from './Modal';
 
 const App = () => {
-  const [ativo, setAtivo] = React.useState(false);
-  const [dados, setDados] = React.useState({ nome: 'Hamilton', idade: '21' });
-
-  function handleClick() {
-    setAtivo(!ativo);
-    setDados({ ...dados, faculdade: 'Possui Faculdade' });
-  }
-
+  const [modal, setModal] = React.useState(false);
   return (
     <div>
-      <p>{dados.nome}</p>
-      <p>{dados.idade}</p>
-      <p>{dados.faculdade}</p>
-      <button onClick={handleClick}>{ativo ? 'Ativo' : 'Inativo'}</button>
+      <Modal modal={modal} setModal={setModal} />
+      <ButtonModal setModal={setModal} />
     </div>
   );
 };
